6af5cb24df44b53565fc74186d82c370913520de
Anurag Pandey <anuragpandey1759@gmail.com>
2026-09-07 14:07:13 +0530

    Time: 2 ms (51.52%) | Memory: 19.6 MB (15.34%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        # Make sure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m = len(nums1)
+        n = len(nums2)
+
+        left = 0
+        right = m
+
+        while left <= right:
+            partition1 = (left + right) // 2
+            partition2 = (m + n + 1) // 2 - partition1
+
+            # Left and right values around partition 1
+            if partition1 == 0:
+                left1 = float('-inf')
+            else:
+                left1 = nums1[partition1 - 1]
+
+            if partition1 == m:
+                right1 = float('inf')
+            else:
+                right1 = nums1[partition1]
+
+            # Left and right values around partition 2
+            if partition2 == 0:
+                left2 = float('-inf')
+            else:
+                left2 = nums2[partition2 - 1]
+
+            if partition2 == n:
+                right2 = float('inf')
+            else:
+                right2 = nums2[partition2]
+
+            # Correct partition found
+            if left1 <= right2 and left2 <= right1:
+
+                # Odd number of elements
+                if (m + n) % 2 == 1:
+                    return float(max(left1, left2))
+
+                # Even number of elements
+                return (max(left1, left2) + min(right1, right2)) / 2.0
+
+            # Move partition to the left
+            elif left1 > right2:
+                right = partition1 - 1
+
+            # Move partition to the right
+            else:
+                left = partition1 + 1
